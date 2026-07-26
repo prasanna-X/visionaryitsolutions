dff0ecb113a03f071e6fb2d9acda7bf1ad9f779e
Prasanna Karki <karkijungprasanna@gmail.com>
2026-07-26 16:41:51 +0545

    Stop tracking .next and node_modules

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"yckSmL9og3JwH+t5qP4sboP5eqxOez7iLyrhibSSAtY=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_0zr2k9a._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/components_1sugmi6._.js","static/chunks/node_modules_1bu7ut0._.js"],
-    source: "dynamic"
-});
