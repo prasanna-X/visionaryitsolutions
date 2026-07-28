d1737463beffc2dfa4cfd1517b6d37c19786c0ea
Prasanna Karki <karkijungprasanna@gmail.com>
2026-07-29 00:06:50 +0545

    team file deleted

--- app/(site)/team/page.tsx
+++
@@ -1,7 +0,0 @@
-// "/team" - public team members list
-import { getAllTeamMembers } from '@/lib/services/teamMemberService';
-
-export default async function TeamPage() {
-  const members = await getAllTeamMembers();
-  return <main>Team Page</main>;
-}
