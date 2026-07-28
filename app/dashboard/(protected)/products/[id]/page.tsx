f83984b093cb830da019464968ca0fd7902a137c
Prasanna Karki <karkijungprasanna@gmail.com>
2026-07-28 23:39:46 +0545

    product detail page bug fix

--- app/dashboard/(protected)/products/[id]/page.tsx
+++ app/dashboard/(protected)/products/[id]/page.tsx
@@ -2,5 +2,5 @@ import { getProductById } from '@/lib/services/productService';
 
 export default async function DashboardProductDetailPage({ params }: { params: { id: string } }) {
   const product = await getProductById(params.id);
-  return <div>{product?.name}</div>;
+  return <div>{product?.title}</div>;
 }
